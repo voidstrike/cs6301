4a915cc2f79eca41ee3decf75413bd6b9b564b88
Alan Lin <linyu0077@gmail.com>
2017-08-27 12:40:05 -0500

    MergeSort first version and gn00 package updated

---
+++ src/cs6301/g00/Shuffle.java
@@ -0,0 +1,58 @@
+
+/** @author rbk : based on algorithm described in a book
+ *  Ver 1.0: 2017/08/08
+ */
+
+package cs6301.g00;
+import java.util.Random;
+
+/* Shuffle the elements of an array arr[from..to] randomly */
+public class Shuffle {
+
+    public static<T> void shuffle(T[] arr) {
+	shuffle(arr, 0, arr.length-1);
+    }
+
+    public static<T> void shuffle(T[] arr, int from, int to) {
+	int n = to - from  + 1;
+	Random rand = new Random();
+	for(int i=1; i<n; i++) {
+	    int j = rand.nextInt(i);
+	    swap(arr, i+from, j+from);
+	}
+    }
+
+    static<T> void swap(T[] arr, int x, int y) {
+	T tmp = arr[x];
+	arr[x] = arr[y];
+	arr[y] = tmp;
+    }
+
+    static<T> void printArray(T[] arr, String message) {
+	printArray(arr, 0, arr.length-1, message);
+    }
+
+    static<T> void printArray(T[] arr, int from, int to, String message) {
+	System.out.print(message);
+	for(int i=from; i<=to; i++) {
+	    System.out.print(" " + arr[i]);
+	}
+	System.out.println();
+    }
+
+    public static void main(String[] args) {
+	int n = 10;
+        Integer[] arr = new Integer[n];
+        for(int i=0; i<n; i++) {
+	    arr[i] = new Integer(i);
+	}
+	printArray(arr, "Before:");
+	shuffle(arr);
+	printArray(arr, "After:");
+    }
+}
+
+/** Sample output:
+Before: 0 1 2 3 4 5 6 7 8 9
+After: 3 7 9 8 2 4 6 1 0 5
+*/
--- src/cs6301/g42/MergeSorter.java
+++ src/cs6301/g42/MergeSorter.java
@@ -4,23 +4,64 @@ package cs6301.g42;
  * Created by Alan Lin on 8/26/2017.
  */
 public class MergeSorter {
-    static <T extends Comparable<? super T>> void mergeSort(T[] arr, T[] tmp)
+
+    public static <T extends Comparable<? super T>> void mergeSortPhase(T[] arr, T[] tmp, int start, int end)
+    {
+        int middle = (start + end) >>> 1;
+        if(start < end)
+        {
+            mergeSortPhase(arr, tmp, start, middle);
+            mergeSortPhase(arr, tmp, middle+1, end);
+            mergeSortMerger(arr, tmp,start, middle, end);
+        }
+    }
+
+    public static <T extends  Comparable<? super T>> void mergeSortMerger(T[] arr, T[] tmp, int start, int middle, int end)
+    {
+        //TODO
+        int arrayLength = end - start + 1;
+        int firstPivot = start, secondPivot = middle + 1;
+
+        for(int i=0; i<arrayLength; i++)
+            tmp[i+start] = arr[i+start];
+        for(int k=0; k<arrayLength; k++)
+        {
+            if(tmp[firstPivot].compareTo(tmp[secondPivot]) == -1 && firstPivot <= middle)
+            {
+                arr[start+k] = tmp[firstPivot];
+                firstPivot++;
+            }
+            else
+            {
+                arr[start+k] = tmp[secondPivot];
+                secondPivot++;
+            }
+        }
+    }
+
+    public static <T extends Comparable<? super T>> void mergeSort(T[] arr, T[] tmp)
+    {
+        //TODO
+        mergeSortPhase(arr, tmp, 0, arr.length-1);
+    }
+
+    public static void mergeSort(int[] arr, int[] tmp)
     {
         //TODO
     }
 
-    static void mergesSort(int[] arr, int[] tmp)
-    {
-        //TODO
-    }
-
-    static <T extends Comparable<? super T>> void nSquareSort(T[] arr)
+    public static <T extends Comparable<? super T>> void nSquareSort(T[] arr)
     {
         //TODO
     }
 
     public static void main(String[] args)
     {
-        //TODO
+        MergeSorter sorter = new MergeSorter();
+        Integer[] test = {1,2,4,3,5};
+        Integer[] tmp = {0,0,0,0,0};
+        mergeSort(test, tmp);
+        for(int i=0; i<test.length;i++)
+            System.out.println(test[i]);
     }
 }
